8e2ab01e79af510ca2e6a8b21c7d26647fd259f6
wallgravve <pawlaczyk.lukasz@gmail.com>
2019-08-03 16:45:33 +0200

    git changed on bootstrap framework

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function ($) {
+	"use strict";
+	var nav = $('nav');
+  var navHeight = nav.outerHeight();
+  
+  $('.navbar-toggler').on('click', function() {
+    if( ! $('#mainNav').hasClass('navbar-reduce')) {
+      $('#mainNav').addClass('navbar-reduce');
+    }
+  })
+
+
+
+})(jQuery);
+
+
